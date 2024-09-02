6f17fa33d4ab49fc3ed4b39c65c0338934801157
Rushikesh Pawar <125253603+Neorex80@users.noreply.github.com>
2024-09-02 19:50:01 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAkVdhAM2Va5sxfO1-qFsNy0AxOmlLiD6w",
-    authDomain: "hackerwolf-82dbc.firebaseapp.com",
-    projectId: "hackerwolf-82dbc",
-    storageBucket: "hackerwolf-82dbc.appspot.com",
-    messagingSenderId: "1017905310519",
-    appId: "1:1017905310519:web:c19e2bdb6dd15c4db988b2",
-    measurementId: "G-Q3PM1JZ8WM"
-  };
+const firebaseConfig = {}
 
 const app = initializeApp(firebaseConfig); 
 const db = getFirestore(app);
